f635c74bf71e72edb3bdf132b3d72a95116fe1e4
Corey Gross <>
2021-02-21 18:44:25 -0500

    Font conversions

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,21 +12,5 @@ export default {
     'gatsby-plugin-sitemap',
     'gatsby-plugin-mdx',
     'gatsby-transformer-sharp',
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'images',
-        path: './src/images/',
-      },
-      __key: 'images',
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'pages',
-        path: './src/pages/',
-      },
-      __key: 'pages',
-    },
   ],
 }
